15a8b11bf96cff4efd016c60bc7ca59628eab719
Miqqueas279 <mservettini279@gmail.com>
2024-10-06 18:56:36 -0300

    Update puntod.sh

--- parcial1/puntod.sh
+++ parcial1/puntod.sh
@@ -1,18 +1,18 @@
 pasos a seguir crear estructrura:
 
-mkdir -p Estructura_Asimetrica/correo
+kdir -p Estructura_Asimetrica/correo
 mkdir -p Estructura_Asimetrica/clientes
 
 # Crear cartas_1 a cartas_100 en ambas carpetas
-for i in {1..100}; do
-    touch Estructura_Asimetrica/correo/cartas$i
-    touch Estructura_Asimetrica/clientes/cartas$i
+for i in $(seq 1 100); do
+  touch Estructura_Asimetrica/correo/carta_$i
+  touch Estructura_Asimetrica/clientes/carta_$i
 done
 
-# Crear carteros_1 a carteros_10 solo en la carpeta correo
-for i in {1..10}; do
-    touch Estructura_Asimetrica/correo/carteros$i
+# Crear carteros_1 a carteros_10 solo en correo
+for i in $(seq 1 10); do
+  touch Estructura_Asimetrica/correo/cartero_$i
 done
 
-revisar en tree:
+revisar con tree:
 tree Estructura_Asimetrica
